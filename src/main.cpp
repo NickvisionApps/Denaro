b30b346be2314c32c4386a7c8818a152d10c6e6c
Enzo MAROS <enzo.maros@gmail.com>
2022-11-05 17:13:34 +0100

    Added gettext dependency
    All translation files will be stored in the po subdirectory. Meson will
    be responsible for creating and updating them by reading :
     - po/LINGUAS to know which language are supported
     - po/POTFILES to know which source files to look at for finding text

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <libintl.h>
 #include "ui/application.hpp"
 
 using namespace NickvisionMoney::UI;
@@ -12,6 +13,11 @@ using namespace NickvisionMoney::UI;
  */
 int main(int argc, char* argv[])
 {
+    setlocale(LC_ALL, "");
+    bindtextdomain(GETTEXT_PACKAGE, LOCALE_DIR);
+    bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+    textdomain(GETTEXT_PACKAGE);
+    
     Application app("org.nickvision.money");
     return app.run(argc, argv);
 }
