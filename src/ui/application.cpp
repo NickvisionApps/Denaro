39e19ce2fd8985505e570548f340f3eaf8864a70
Nick Logozzo <nlogozzo225@gmail.com>
2022-11-06 08:41:15 -0500

    Update Changelog

--- src/ui/application.cpp
+++ src/ui/application.cpp
@@ -15,7 +15,7 @@ Application::Application(const std::string& id, GApplicationFlags flags) : m_adw
     m_appInfo.setShortName("Money");
     m_appInfo.setDescription(_("A personal finance manager."));
     m_appInfo.setVersion("2022.11.0-beta2");
-    m_appInfo.setChangelog("<ul><li>Introducing Groups: Add groups to an account and associate transactions with groups for a more precise finance management system</li><li>Added support for translations</li></ul>");
+    m_appInfo.setChangelog("<ul><li>Introducing Groups: Add groups to an account and associate transactions with groups for a more precise finance management system</li><li>Added support for translations</li><li>Added french translation (Thanks @zothma!)</li></ul>");
     m_appInfo.setGitHubRepo("https://github.com/nlogozzo/NickvisionMoney");
     m_appInfo.setIssueTracker("https://github.com/nlogozzo/NickvisionMoney/issues/new");
     m_appInfo.setSupportUrl("https://github.com/nlogozzo/NickvisionMoney/discussions");
