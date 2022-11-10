31a21e1c486829a4134544d008883032799df9fe
Nick Logozzo <nlogozzo225@gmail.com>
2022-11-09 23:24:44 -0500

    Start MoneyHelpers

---
+++ src/helpers/moneyhelpers.cpp
@@ -0,0 +1,13 @@
+#include "moneyhelpers.hpp"
+
+using namespace NickvisionMoney::Helpers;
+
+std::string MoneyHelpers::boostMoneyToLocaleString(boost::multiprecision::cpp_dec_float_50 amount, const std::locale& locale)
+{
+    return "";
+}
+
+boost::multiprecision::cpp_dec_float_50 MoneyHelpers::localeStringToBoostMoney(const std::string& localeString, const std::locale& locale)
+{
+    return 0.0;
+}
---
+++ src/helpers/moneyhelpers.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <locale>
+#include <string>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
+namespace NickvisionMoney::Helpers::MoneyHelpers
+{
+	std::string boostMoneyToLocaleString(boost::multiprecision::cpp_dec_float_50 amount, const std::locale& locale);
+	boost::multiprecision::cpp_dec_float_50 localeStringToBoostMoney(const std::string& localeString, const std::locale& locale);
+}
