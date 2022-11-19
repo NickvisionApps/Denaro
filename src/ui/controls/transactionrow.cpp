2b2f92c34b59034359dce72a75a28b71fcf40ae1
Nick Logozzo <nlogozzo225@gmail.com>
2022-11-19 10:56:28 -0500

    Fix TransactionRow Activation

--- src/ui/controls/transactionrow.cpp
+++ src/ui/controls/transactionrow.cpp
@@ -8,12 +8,13 @@ using namespace NickvisionMoney::Helpers;
 using namespace NickvisionMoney::Models;
 using namespace NickvisionMoney::UI::Controls;
 
-TransactionRow::TransactionRow(const Transaction& transaction, const std::locale& locale) : m_transaction{ transaction }, m_gobj{ adw_action_row_new() }
+TransactionRow::TransactionRow(const Transaction& transaction, const std::locale& locale) : m_transaction{ transaction }, m_gobj{ adw_preferences_group_new() }
 {
     //Row Settings
+    m_row = adw_action_row_new();
     std::stringstream builder;
     builder << m_transaction.getDescription();
-    adw_preferences_row_set_title(ADW_PREFERENCES_ROW(m_gobj), builder.str().c_str());
+    adw_preferences_row_set_title(ADW_PREFERENCES_ROW(m_row), builder.str().c_str());
     builder.str("");
     builder.clear();
     builder << g_date_time_format(g_date_time_new_local(m_transaction.getDate().year(), m_transaction.getDate().month(), m_transaction.getDate().day(), 0, 0, 0.0), "%x");
@@ -21,9 +22,8 @@ TransactionRow::TransactionRow(const Transaction& transaction, const std::locale
     {
         builder << "\n" << _("Repeat Interval: ") << m_transaction.getRepeatIntervalAsString();
     }
-    adw_action_row_set_subtitle(ADW_ACTION_ROW(m_gobj), builder.str().c_str());
-    gtk_widget_add_css_class(m_gobj, "card");
-    gtk_widget_set_size_request(m_gobj, 250, -1);
+    adw_action_row_set_subtitle(ADW_ACTION_ROW(m_row), builder.str().c_str());
+    gtk_widget_set_size_request(m_row, 300, -1);
     //Button ID
     m_btnId = gtk_button_new();
     gtk_widget_set_name(m_btnId, "btnId");
@@ -45,7 +45,7 @@ TransactionRow::TransactionRow(const Transaction& transaction, const std::locale
     cssBtnId += ";\n}";
     gtk_css_provider_load_from_data(cssProviderBtnId, cssBtnId.c_str(), -1);
     gtk_style_context_add_provider(gtk_widget_get_style_context(m_btnId), GTK_STYLE_PROVIDER(cssProviderBtnId),  GTK_STYLE_PROVIDER_PRIORITY_USER);
-    adw_action_row_add_prefix(ADW_ACTION_ROW(m_gobj), m_btnId);
+    adw_action_row_add_prefix(ADW_ACTION_ROW(m_row), m_btnId);
     //Amount Label
     m_lblAmount = gtk_label_new(MoneyHelpers::boostMoneyToLocaleString(m_transaction.getAmount(), locale).c_str());
     gtk_widget_add_css_class(m_lblAmount, m_transaction.getType() == TransactionType::Income ? "success" : "error");
@@ -55,7 +55,7 @@ TransactionRow::TransactionRow(const Transaction& transaction, const std::locale
     gtk_widget_add_css_class(m_btnEdit, "flat");
     gtk_button_set_icon_name(GTK_BUTTON(m_btnEdit), "edit-symbolic");
     gtk_widget_set_tooltip_text(m_btnEdit, _("Edit Transaction"));
-    adw_action_row_set_activatable_widget(ADW_ACTION_ROW(m_gobj), m_btnEdit);
+    adw_action_row_set_activatable_widget(ADW_ACTION_ROW(m_row), m_btnEdit);
     g_signal_connect(m_btnEdit, "clicked", G_CALLBACK((void (*)(GtkButton*, gpointer))[](GtkButton*, gpointer data) { reinterpret_cast<TransactionRow*>(data)->onEdit(); }), this);
     //Delete Button
     m_btnDelete = gtk_button_new();
@@ -69,11 +69,9 @@ TransactionRow::TransactionRow(const Transaction& transaction, const std::locale
     gtk_box_append(GTK_BOX(m_box), m_lblAmount);
     gtk_box_append(GTK_BOX(m_box), m_btnEdit);
     gtk_box_append(GTK_BOX(m_box), m_btnDelete);
-    adw_action_row_add_suffix(ADW_ACTION_ROW(m_gobj), m_box);
-    //Mouse Click Gesture
-    m_gestureMouseClick = gtk_gesture_click_new();
-    g_signal_connect(m_gestureMouseClick, "pressed", G_CALLBACK((void (*)(GtkGesture*, int, double, double, gpointer))[](GtkGesture*, int, double, double, gpointer data) { adw_action_row_activate(ADW_ACTION_ROW(reinterpret_cast<TransactionRow*>(data)->m_gobj)); }), this);
-    gtk_widget_add_controller(m_gobj, GTK_EVENT_CONTROLLER(m_gestureMouseClick));
+    adw_action_row_add_suffix(ADW_ACTION_ROW(m_row), m_box);
+    //Group Settings
+    adw_preferences_group_add(ADW_PREFERENCES_GROUP(m_gobj), m_row);
 }
 
 GtkWidget* TransactionRow::gobj()
