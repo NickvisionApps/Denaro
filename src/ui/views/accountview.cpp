397980b17ddf0bb3d70a1c516693c3b95847085c
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2022-11-21 16:31:23 +0300

    Improve layout

--- src/ui/views/accountview.cpp
+++ src/ui/views/accountview.cpp
@@ -212,12 +212,14 @@ AccountView::AccountView(GtkWindow* parentWindow, AdwTabView* parentTabView, Gtk
     gtk_flow_box_set_homogeneous(GTK_FLOW_BOX(m_flowBox), true);
     gtk_flow_box_set_column_spacing(GTK_FLOW_BOX(m_flowBox), 10);
     gtk_flow_box_set_row_spacing(GTK_FLOW_BOX(m_flowBox), 10);
+    gtk_widget_set_margin_bottom(m_flowBox, 60);
     gtk_widget_set_halign(m_flowBox, GTK_ALIGN_FILL);
     gtk_widget_set_valign(m_flowBox, GTK_ALIGN_START);
     gtk_flow_box_set_selection_mode(GTK_FLOW_BOX(m_flowBox), GTK_SELECTION_NONE);
     //Transactions Scrolled Window
     m_scrollTransactions = gtk_scrolled_window_new();
     gtk_widget_set_size_request(m_scrollTransactions, 300, -1);
+    gtk_scrolled_window_set_min_content_height(GTK_SCROLLED_WINDOW(m_scrollTransactions), 135);
     gtk_widget_set_vexpand(m_scrollTransactions, true);
     gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(m_scrollTransactions), m_flowBox);
     //Main Box
