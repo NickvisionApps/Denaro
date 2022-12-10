77e7f161e09595afbd9ff13d18b2b1da13b8f877
Nick Logozzo <nlogozzo225@gmail.com>
2022-12-10 01:20:08 -0500

    Shared - Transaction Model

---
+++ NickvisionMoney.Shared/Models/TransactionRepeatInterval.cs
@@ -0,0 +1,16 @@
+﻿namespace NickvisionMoney.Shared.Models;
+
+/// <summary>
+/// Repeat intervals for a transaction
+/// </summary>
+public enum TransactionRepeatInterval
+{
+    Never = 0,
+    Daily,
+    Weekly,
+    Biweekly,
+    Monthly,
+    Quarterly,
+    Yearly,
+    Biyearly
+}
---
+++ NickvisionMoney.Shared/Models/TransactionType.cs
@@ -0,0 +1,10 @@
+﻿namespace NickvisionMoney.Shared.Models;
+
+/// <summary>
+/// Types of a transaction
+/// </summary>
+public enum TransactionType
+{
+    Income = 0,
+    Expense
+}
