857afec642ef548f1d1c7a2ea2c5fe091a11cb77
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2022-12-11 04:37:04 +0300

    Update install.sh

--- NickvisionMoney.GNOME/install.sh
+++ NickvisionMoney.GNOME/install.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
-# This script should be called for the root of the repo
 
-PREFIX="/usr"
-
+INSTALL_PREFIX="/usr"
 if [ ! -z $1 ]
 then
-	PREFIX=$1
+	INSTALL_PREFIX=$1
 fi
-echo Install prefix: $PREFIX
+echo Install prefix: $INSTALL_PREFIX
+
+SOURCE_PREFIX="."
+if [ ${PWD##*/} == "Nickvision.GNOME" ]
+then
+	SOURCE_PREFIX=".."
+fi
 
 echo Installing icons...
-mkdir -p $PREFIX/share/icons/hicolor/scalable/apps
-cp NickvisionMoney.Shared/Resources/org.nickvision.money.svg $PREFIX/share/icons/hicolor/scalable/apps/
-cp NickvisionMoney.Shared/Resources/org.nickvision.money-devel.svg $PREFIX/share/icons/hicolor/scalable/apps/
-mkdir -p $PREFIX/share/icons/hicolor/symbolic/apps
-cp NickvisionMoney.Shared/Resources/bank-symbolic.svg $PREFIX/share/icons/hicolor/symbolic/apps/
-cp NickvisionMoney.Shared/Resources/larger-brush-symbolic.svg $PREFIX/share/icons/hicolor/symbolic/apps/
-cp NickvisionMoney.Shared/Resources/money-none-symbolic.svg $PREFIX/share/icons/hicolor/symbolic/apps/
-cp NickvisionMoney.Shared/Resources/org.nickvision.money-symbolic.svg $PREFIX/share/icons/hicolor/symbolic/apps/
-cp NickvisionMoney.Shared/Resources/wallet2-symbolic.svg $PREFIX/share/icons/hicolor/symbolic/apps/
+mkdir -p $INSTALL_PREFIX/share/icons/hicolor/scalable/apps
+cp $SOURCE_PREFIX/NickvisionMoney.Shared/Resources/org.nickvision.money.svg $INSTALL_PREFIX/share/icons/hicolor/scalable/apps/
+cp $SOURCE_PREFIX/NickvisionMoney.Shared/Resources/org.nickvision.money-devel.svg $INSTALL_PREFIX/share/icons/hicolor/scalable/apps/
+mkdir -p $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps
+cp $SOURCE_PREFIX/NickvisionMoney.Shared/Resources/bank-symbolic.svg $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps/
+cp $SOURCE_PREFIX/NickvisionMoney.Shared/Resources/larger-brush-symbolic.svg $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps/
+cp $SOURCE_PREFIX/NickvisionMoney.Shared/Resources/money-none-symbolic.svg $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps/
+cp $SOURCE_PREFIX/NickvisionMoney.Shared/Resources/org.nickvision.money-symbolic.svg $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps/
+cp $SOURCE_PREFIX/NickvisionMoney.Shared/Resources/wallet2-symbolic.svg $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps/
 
 echo Installing GResource...
-mkdir -p $PREFIX/share/org.nickvision.money
-glib-compile-resources NickvisionMoney.GNOME/Resources/org.nickvision.money.gresource.xml
-mv NickvisionMoney.GNOME/Resources/org.nickvision.money.gresource $PREFIX/share/org.nickvision.money/
+mkdir -p $INSTALL_PREFIX/share/org.nickvision.money
+glib-compile-resources $SOURCE_PREFIX/NickvisionMoney.GNOME/Resources/org.nickvision.money.gresource.xml
+mv $SOURCE_PREFIX/NickvisionMoney.GNOME/Resources/org.nickvision.money.gresource $INSTALL_PREFIX/share/org.nickvision.money/
 
 echo Installing desktop file...
-mkdir -p $PREFIX/share/applications
-cp NickvisionMoney.GNOME/org.nickvision.money.desktop $PREFIX/share/applications/
+mkdir -p $INSTALL_PREFIX/share/applications
+cp $SOURCE_PREFIX/NickvisionMoney.GNOME/org.nickvision.money.desktop $INSTALL_PREFIX/share/applications/
 
 echo Installing metainfo...
-mkdir -p $PREFIX/share/metainfo
-cp NickvisionMoney.GNOME/org.nickvision.money.metainfo.xml $PREFIX/share/metainfo/
+mkdir -p $INSTALL_PREFIX/share/metainfo
+cp $SOURCE_PREFIX/NickvisionMoney.GNOME/org.nickvision.money.metainfo.xml $INSTALL_PREFIX/share/metainfo/
 
 echo Done!
