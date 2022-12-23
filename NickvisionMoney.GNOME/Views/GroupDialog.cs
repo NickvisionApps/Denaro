24c56e4f31999ffe338e4e4692abc6482c5c9c12
Nick Logozzo <nlogozzo225@gmail.com>
2022-12-22 19:48:55 -0500

    GNOME - Use Adw.ActionRow and Gtk.Entry in GroupDialog

--- NickvisionMoney.GNOME/Views/GroupDialog.cs
+++ NickvisionMoney.GNOME/Views/GroupDialog.cs
@@ -2,6 +2,7 @@ using NickvisionMoney.Shared.Controllers;
 using NickvisionMoney.Shared.Helpers;
 using System;
 using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 namespace NickvisionMoney.GNOME.Views;
 
@@ -23,9 +24,6 @@ public partial class GroupDialog
     private static partial nint g_main_context_default();
 
     [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial nint adw_entry_row_new();
-
-    [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
     private static partial void adw_message_dialog_add_response(nint dialog, string id, string label);
 
     [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
@@ -44,35 +42,14 @@ public partial class GroupDialog
     private static partial void adw_message_dialog_set_response_appearance(nint dialog, string response, int appearance);
 
     [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void adw_preferences_group_add(nint group, nint child);
-
-    [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void adw_preferences_row_set_title(nint row, string title);
-
-    [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial string gtk_editable_get_text(nint editable);
-
-    [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void gtk_editable_set_text(nint editable, string text);
-
-    [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void gtk_widget_add_css_class(nint widget, string cssClass);
-
-    [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void gtk_widget_grab_focus(nint widget);
-
-    [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
     [return: MarshalAs(UnmanagedType.I1)]
     private static partial bool gtk_widget_is_visible(nint widget);
-
-    [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void gtk_widget_remove_css_class(nint widget, string cssClass);
 
     [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
     private static partial void gtk_widget_show(nint widget);
 
     [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void gtk_window_close(nint window);
+    private static partial void gtk_window_destroy(nint window);
 
     [LibraryImport("adwaita-1", StringMarshalling = StringMarshalling.Utf8)]
     private static partial void gtk_window_set_default_size(nint window, int x, int y);
@@ -87,8 +64,10 @@ public partial class GroupDialog
     private readonly Localizer _localizer;
     private readonly nint _dialog;
     private readonly Adw.PreferencesGroup _grpGroup;
-    private readonly nint _rowName;
-    private readonly nint _rowDescription;
+    private readonly Adw.ActionRow _rowName;
+    private readonly Gtk.Entry _txtName;
+    private readonly Adw.ActionRow _rowDescription;
+    private readonly Gtk.Entry _txtDescription;
 
     /// <summary>
     /// Constructs a GroupDialog
@@ -113,18 +92,26 @@ public partial class GroupDialog
         //Preferences Group
         _grpGroup = Adw.PreferencesGroup.New();
         //Name
-        _rowName = adw_entry_row_new();
-        adw_preferences_row_set_title(_rowName, _localizer["Name", "Field"]);
-        adw_preferences_group_add(_grpGroup.Handle, _rowName);
+        _rowName = Adw.ActionRow.New();
+        _rowName.SetTitle(_localizer["Name", "Field"]);
+        _txtName = Gtk.Entry.New();
+        _txtName.SetValign(Gtk.Align.Center);
+        _txtName.SetPlaceholderText(_localizer["Name", "Placeholder"]);
+        _rowName.AddSuffix(_txtName);
+        _grpGroup.Add(_rowName);
         //Description
-        _rowDescription = adw_entry_row_new();
-        adw_preferences_row_set_title(_rowDescription, _localizer["Description", "Field"]);
-        adw_preferences_group_add(_grpGroup.Handle, _rowDescription);
+        _rowDescription = Adw.ActionRow.New();
+        _rowDescription.SetTitle(_localizer["Description", "Field"]);
+        _txtDescription = Gtk.Entry.New();
+        _txtDescription.SetValign(Gtk.Align.Center);
+        _txtDescription.SetPlaceholderText(_localizer["Description", "Placeholder"]);
+        _rowDescription.AddSuffix(_txtDescription);
+        _grpGroup.Add(_rowDescription);
         //Layout
         adw_message_dialog_set_extra_child(_dialog, _grpGroup.Handle);
         //Load Group
-        gtk_editable_set_text(_rowName, _controller.Group.Name);
-        gtk_editable_set_text(_rowDescription, _controller.Group.Description);
+        _txtName.SetText(_controller.Group.Name);
+        _txtDescription.SetText(_controller.Group.Description);
     }
 
     /// <summary>
@@ -135,7 +122,7 @@ public partial class GroupDialog
     {
         gtk_widget_show(_dialog);
         gtk_window_set_modal(_dialog, true);
-        gtk_widget_grab_focus(_rowName);
+        _rowName.GrabFocus();
         while(gtk_widget_is_visible(_dialog))
         {
             g_main_context_iteration(g_main_context_default(), false);
@@ -143,36 +130,33 @@ public partial class GroupDialog
         if(_controller.Accepted)
         {
             gtk_window_set_modal(_dialog, false);
-            var status = _controller.UpdateGroup(gtk_editable_get_text(_rowName), gtk_editable_get_text(_rowDescription));
+            var status = _controller.UpdateGroup(_txtName.GetText(), _txtDescription.GetText());
             if(status != GroupCheckStatus.Valid)
             {
-                gtk_widget_remove_css_class(_rowName, "error");
-                adw_preferences_row_set_title(_rowName, _localizer["Name", "Field"]);
-                gtk_widget_remove_css_class(_rowDescription, "error");
-                adw_preferences_row_set_title(_rowDescription, _localizer["Description", "Field"]);
+                _rowName.RemoveCssClass("error");
+                _rowName.SetTitle(_localizer["Name", "Field"]);
+                _rowDescription.RemoveCssClass("error");
+                _rowDescription.SetTitle(_localizer["Description", "Field"]);
                 //Mark Error
-                if(status == GroupCheckStatus.EmptyName)
+                if (status == GroupCheckStatus.EmptyName)
                 {
-                    gtk_widget_add_css_class(_rowName, "error");
-                    adw_preferences_row_set_title(_rowName, _localizer["Name", "Empty"]);
+                    _rowName.AddCssClass("error");
+                    _rowName.SetTitle(_localizer["Name", "Empty"]);
                 }
                 else if(status == GroupCheckStatus.EmptyDescription)
                 {
-                    gtk_widget_add_css_class(_rowDescription, "error");
-                    adw_preferences_row_set_title(_rowDescription, _localizer["Description", "Empty"]);
+                    _rowDescription.AddCssClass("error");
+                    _rowDescription.SetTitle(_localizer["Description", "Empty"]);
                 }
                 else if(status == GroupCheckStatus.NameExists)
                 {
-                    gtk_widget_add_css_class(_rowName, "error");
-                    adw_preferences_row_set_title(_rowName, _localizer["Name", "Exists"]);
+                    _rowName.AddCssClass("error");
+                    _rowName.SetTitle(_localizer["Name", "Exists"]);
                 }
                 return Run();
             }
-            return true;
         }
-        else
-        {
-            return false;
-        }
+        gtk_window_destroy(_dialog);
+        return _controller.Accepted;
     }
 }
