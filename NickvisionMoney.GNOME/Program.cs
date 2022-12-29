7488ab6abc71d7bbdbe8824ac5b0c2feb9d493b8
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2022-12-29 22:14:31 +0300

    GNOME - Fix gresource again

--- NickvisionMoney.GNOME/Program.cs
+++ NickvisionMoney.GNOME/Program.cs
@@ -2,6 +2,7 @@
 using NickvisionMoney.Shared.Controllers;
 using NickvisionMoney.Shared.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -35,15 +36,18 @@ public class Program
         Adw.Module.Initialize();
         _application = Adw.Application.New("org.nickvision.money", Gio.ApplicationFlags.FlagsNone);
         _application.OnActivate += OnActivate;
-        try
+        var prefixes = new List<string> {
+            Directory.GetParent(Directory.GetParent(Path.GetFullPath(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))).FullName).FullName,
+            Directory.GetParent(Path.GetFullPath(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))).FullName,
+            "/usr"
+        };
+        foreach(var prefix in prefixes)
         {
-            var prefix = Directory.GetParent(Directory.GetParent(Path.GetFullPath(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))).FullName).FullName;
-            g_resources_register(g_resource_load(Path.GetFullPath(prefix + "/share/org.nickvision.money/org.nickvision.money.gresource")));
-        }
-        catch
-        {
-            var prefix = Directory.GetParent(Path.GetFullPath(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))).FullName;
-            g_resources_register(g_resource_load(Path.GetFullPath(prefix + "/share/org.nickvision.money/org.nickvision.money.gresource")));
+            if(File.Exists(prefix + "/share/org.nickvision.money/org.nickvision.money.gresource"))
+            {
+                g_resources_register(g_resource_load(Path.GetFullPath(prefix + "/share/org.nickvision.money/org.nickvision.money.gresource")));
+                break;
+            }
         }
     }
 
