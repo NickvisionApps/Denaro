d4222a13288cfdc1da047f35459a3d22b0178dce
Nick Logozzo <nlogozzo225@gmail.com>
2023-01-04 10:23:27 -0500

    WinUI - Add RepeatEndDate

--- NickvisionMoney.WinUI/Views/TransactionDialog.xaml.cs
+++ NickvisionMoney.WinUI/Views/TransactionDialog.xaml.cs
@@ -42,6 +42,8 @@ public sealed partial class TransactionDialog : ContentDialog
         CmbType.Items.Add(_controller.Localizer["Income"]);
         CmbType.Items.Add(_controller.Localizer["Expense"]);
         CalendarDate.Header = _controller.Localizer["Date", "Field"];
+        CmbGroup.Header = _controller.Localizer["Group", "Field"];
+        CmbGroup.Items.Add(_controller.Localizer["Ungrouped"]);
         CmbRepeatInterval.Header = _controller.Localizer["TransactionRepeatInterval", "Field"];
         CmbRepeatInterval.Items.Add(_controller.Localizer["RepeatInterval", "Never"]);
         CmbRepeatInterval.Items.Add(_controller.Localizer["RepeatInterval", "Daily"]);
@@ -51,8 +53,8 @@ public sealed partial class TransactionDialog : ContentDialog
         CmbRepeatInterval.Items.Add(_controller.Localizer["RepeatInterval", "Quarterly"]);
         CmbRepeatInterval.Items.Add(_controller.Localizer["RepeatInterval", "Yearly"]);
         CmbRepeatInterval.Items.Add(_controller.Localizer["RepeatInterval", "Biyearly"]);
-        CmbGroup.Header = _controller.Localizer["Group", "Field"];
-        CmbGroup.Items.Add(_controller.Localizer["Ungrouped"]);
+        CalendarRepeatEndDate.Header = _controller.Localizer["TransactionRepeatEndDate", "Field"];
+        ToolTipService.SetToolTip(BtnRepeatEndDateClear, controller.Localizer["TransactionRepeatEndDate", "Clear"]);
         LblColor.Text = _controller.Localizer["Color", "Field"];
         LblReceipt.Text = _controller.Localizer["Receipt", "Field"];
         LblBtnReceiptView.Text = _controller.Localizer["View"];
@@ -64,7 +66,6 @@ public sealed partial class TransactionDialog : ContentDialog
         TxtAmount.Text = _controller.Transaction.Amount.ToString("C");
         CmbType.SelectedIndex = (int)_controller.Transaction.Type;
         CalendarDate.Date = new DateTimeOffset(new DateTime(_controller.Transaction.Date.Year, _controller.Transaction.Date.Month, _controller.Transaction.Date.Day));
-        CmbRepeatInterval.SelectedIndex = (int)_controller.Transaction.RepeatInterval;
         var groups = _controller.Groups.Values.ToList();
         groups.Sort();
         foreach (var group in groups)
@@ -78,6 +79,13 @@ public sealed partial class TransactionDialog : ContentDialog
         else
         {
             CmbGroup.SelectedItem = _controller.Groups[(uint)_controller.Transaction.GroupId];
+        }
+        CmbRepeatInterval.SelectedIndex = (int)_controller.Transaction.RepeatInterval;
+        CalendarRepeatEndDate.IsEnabled = _controller.Transaction.RepeatInterval != TransactionRepeatInterval.Never;
+        BtnRepeatEndDateClear.IsEnabled = _controller.Transaction.RepeatInterval != TransactionRepeatInterval.Never;
+        if (_controller.Transaction.RepeatEndDate != null)
+        {
+            CalendarRepeatEndDate.Date = new DateTimeOffset(new DateTime(_controller.Transaction.RepeatEndDate.Value.Year, _controller.Transaction.RepeatEndDate.Value.Month, _controller.Transaction.RepeatEndDate.Value.Day));
         }
         BtnColor.SelectedColor = (Windows.UI.Color)(ColorHelpers.FromRGBA(_controller.Transaction.RGBA) ?? ColorHelpers.FromRGBA(_controller.TransactionDefaultColor)!);
         BtnReceiptView.IsEnabled = _controller.Transaction.Receipt != null;
@@ -98,7 +106,7 @@ public sealed partial class TransactionDialog : ContentDialog
         }
         else if (result == ContentDialogResult.Primary)
         {
-            var checkStatus = await _controller.UpdateTransactionAsync(DateOnly.FromDateTime(CalendarDate.Date!.Value.Date), TxtDescription.Text, (TransactionType)CmbType.SelectedIndex, CmbRepeatInterval.SelectedIndex, (string)CmbGroup.SelectedItem, ColorHelpers.ToRGBA(BtnColor.SelectedColor), TxtAmount.Text, _receiptPath);
+            var checkStatus = await _controller.UpdateTransactionAsync(DateOnly.FromDateTime(CalendarDate.Date!.Value.Date), TxtDescription.Text, (TransactionType)CmbType.SelectedIndex, CmbRepeatInterval.SelectedIndex, (string)CmbGroup.SelectedItem, ColorHelpers.ToRGBA(BtnColor.SelectedColor), TxtAmount.Text, _receiptPath, CalendarRepeatEndDate.Date == null ? null : DateOnly.FromDateTime(CalendarRepeatEndDate.Date!.Value.Date));
             if(checkStatus != TransactionCheckStatus.Valid)
             {
                 TxtDescription.Header = _controller.Localizer["Description", "Field"];
@@ -122,6 +130,25 @@ public sealed partial class TransactionDialog : ContentDialog
         }
         return false;
     }
+
+    /// <summary>
+    /// Occurs when the repeat interval combobox is changed
+    /// </summary>
+    /// <param name="sender">object</param>
+    /// <param name="e">SelectionChangedEventArgs</param>
+    private void CmbRepeatInterval_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var isRepeatIntervalNever = (string)CmbRepeatInterval.SelectedItem == _controller.Localizer["RepeatInterval", "Never"];
+        CalendarRepeatEndDate.IsEnabled = !isRepeatIntervalNever;
+        BtnRepeatEndDateClear.IsEnabled = !isRepeatIntervalNever;
+    }
+
+    /// <summary>
+    /// Occurs when the clear repeat end date button is clicked
+    /// </summary>
+    /// <param name="sender">object</param>
+    /// <param name="e">RoutedEventArgs</param>
+    private void ClearRepeatEndDate(object sender, RoutedEventArgs e) => CalendarRepeatEndDate.Date = null;
 
     /// <summary>
     /// Occurs when the view receipt button is clicked
