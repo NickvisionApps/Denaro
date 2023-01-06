77dd4fbbf27ed26e616d30b1a62d1990f3dd5609
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-01-06 23:18:21 +0300

    Shared - Add localizer fallback

--- NickvisionMoney.Shared/Helpers/Localizer.cs
+++ NickvisionMoney.Shared/Helpers/Localizer.cs
@@ -10,6 +10,7 @@ public class Localizer
 {
     private readonly ResourceManager _resourceManager;
     private readonly ResourceSet _resourceSet;
+    private readonly ResourceSet _resourceFallback;
 
     /// <summary>
     /// Gets a localized non-plural/plural string
@@ -33,6 +34,7 @@ public class Localizer
     {
         _resourceManager = new ResourceManager("NickvisionMoney.Shared.Resources.Strings", GetType().Assembly);
         _resourceSet = _resourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true)!;
+        _resourceFallback = _resourceManager.GetResourceSet(new CultureInfo("en-US"), true, true)!;
     }
 
     /// <summary>
@@ -40,7 +42,7 @@ public class Localizer
     /// </summary>
     /// <param name="name">The name of the string resource</param>
     /// <returns>The localized string</returns>
-    public string GetString(string name) => _resourceSet.GetString(name) ?? string.Empty;
+    public string GetString(string name) => _resourceSet.GetString(name) ?? _resourceFallback.GetString(name) ?? string.Empty;
 
     /// <summary>
     /// Gets a localized string by context
@@ -48,12 +50,12 @@ public class Localizer
     /// <param name="name">The name of the string resource</param>
     /// <param name="context">The name of the context</param>
     /// <returns>The localized string with the context</returns>
-    public string GetStringWithContext(string name, string context) => _resourceSet.GetString($"{name}.{context}") ?? string.Empty;
+    public string GetStringWithContext(string name, string context) => GetString($"{name}.{context}");
 
     /// <summary>
     /// Gets a localized plural string
     /// </summary>
     /// <param name="name">The name of the string resource</param>
     /// <returns>The localized plural string</returns>
-    public string GetPluralString(string name) => _resourceSet.GetString($"{name}.Plural") ?? string.Empty;
+    public string GetPluralString(string name) => GetString($"{name}.Plural");
 }
