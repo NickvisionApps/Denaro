59b5d9b8fe5db5c80f87c0d779df75b948c06f1b
Nick Logozzo <nlogozzo225@gmail.com>
2023-01-07 08:59:12 -0500

    GNOME - Cleanup

--- NickvisionMoney.GNOME/Views/AccountSettingsDialog.cs
+++ NickvisionMoney.GNOME/Views/AccountSettingsDialog.cs
@@ -268,7 +268,6 @@ public partial class AccountSettingsDialog
         var bgColorString = _controller.GetColorForAccountType((AccountType)_rowAccountType.GetSelected());
         var bgColorStrArray = new Regex(@"[0-9]+,[0-9]+,[0-9]+").Match(bgColorString).Value.Split(",");
         var luma = int.Parse(bgColorStrArray[0]) / 255 * 0.2126 + int.Parse(bgColorStrArray[1]) / 255 * 0.7152 + int.Parse(bgColorStrArray[2]) / 255 * 0.0722;
-
         _btnAvatar.GetStyleContext().RemoveProvider(_btnAvatarCssProvider);
         var btnCss = "#btnAvatar { color: " + (luma < 0.5 ? "#fff" : "#000") + "; background-color: " + bgColorString + "; }" + char.MinValue;
         gtk_css_provider_load_from_data(_btnAvatarCssProvider.Handle, btnCss, -1);
