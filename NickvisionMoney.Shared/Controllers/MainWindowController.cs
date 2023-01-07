a78b9ab6a9b4b36202a73e8088cd163f6539b45b
Nick Logozzo <nlogozzo225@gmail.com>
2023-01-07 09:24:41 -0500

    Shared - Add MainWindowController.GetColorForAccountType

--- NickvisionMoney.Shared/Controllers/MainWindowController.cs
+++ NickvisionMoney.Shared/Controllers/MainWindowController.cs
@@ -109,6 +109,22 @@ public class MainWindowController
     }
 
     /// <summary>
+    /// Gets a color for an account type
+    /// </summary>
+    /// <param name="accountType">The account type</param>
+    /// <returns>The rgb color for the account type</returns>
+    public string GetColorForAccountType(AccountType accountType)
+    {
+        return accountType switch
+        {
+            AccountType.Checking => Configuration.Current.AccountCheckingColor,
+            AccountType.Savings => Configuration.Current.AccountSavingsColor,
+            AccountType.Business => Configuration.Current.AccountBusinessColor,
+            _ => Configuration.Current.AccountSavingsColor
+        };
+    }
+
+    /// <summary>
     /// Gets whether or not an account with the given path is opened or not
     /// </summary>
     /// <param name="path">The path of the account to check</param>
