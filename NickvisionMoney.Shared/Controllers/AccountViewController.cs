04841d26c9d88903708c59c8218538adfa52196c
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-01-30 03:03:49 +0300

    GNOME - Import help when 0 transactions imported

--- NickvisionMoney.Shared/Controllers/AccountViewController.cs
+++ NickvisionMoney.Shared/Controllers/AccountViewController.cs
@@ -816,7 +816,7 @@ public class AccountViewController
                 GroupRows[groupId].UpdateRow(_account.Groups[groupId], CultureForNumberString, CultureForDateString, _filters[(int)groupId]);
             }    
             FilterUIUpdate();
-            NotificationSent?.Invoke(this, new NotificationSentEventArgs(importedIds.Count == 1 ? string.Format(Localizer["Imported"], importedIds.Count) : string.Format(Localizer["Imported", true], importedIds.Count), NotificationSeverity.Success));
+            NotificationSent?.Invoke(this, new NotificationSentEventArgs(importedIds.Count == 1 ? string.Format(Localizer["Imported"], importedIds.Count) : string.Format(Localizer["Imported", true], importedIds.Count), NotificationSeverity.Success, importedIds.Count == 0 ? "help-import" : ""));
         }
         else
         {
