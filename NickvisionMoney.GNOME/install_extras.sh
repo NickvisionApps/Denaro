7de9a29807cc8207119c0de5e5a3b6658ea86ea2
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-02-10 00:29:44 +0300

    GNOME - Update metadata

--- NickvisionMoney.GNOME/install_extras.sh
+++ NickvisionMoney.GNOME/install_extras.sh
@@ -42,17 +42,17 @@ echo "Installing GResource..."
 mkdir -p "${INSTALL_PREFIX}"/share/org.nickvision.money
 glib-compile-resources ./NickvisionMoney.GNOME/Resources/org.nickvision.money.gresource.xml
 mv ./NickvisionMoney.GNOME/Resources/org.nickvision.money.gresource \
-   "${INSTALL_PREFIX}"/share/org.nickvision.money/
+    "${INSTALL_PREFIX}"/share/org.nickvision.money/
 
 echo "Installing desktop file..."
 mkdir -p "${INSTALL_PREFIX}"/share/applications
 cp ./NickvisionMoney.GNOME/org.nickvision.money.desktop \
-   "${INSTALL_PREFIX}"/share/applications/
+    "${INSTALL_PREFIX}"/share/applications/
 
 echo "Installing metainfo..."
 mkdir -p "${INSTALL_PREFIX}"/share/metainfo
 cp ./NickvisionMoney.GNOME/org.nickvision.money.metainfo.xml    \
-   "${INSTALL_PREFIX}"/share/metainfo/
+    "${INSTALL_PREFIX}"/share/metainfo/
 
 echo "Translating desktop file and metainfo..."
 python3 ./NickvisionMoney.GNOME/translate_meta.py ${INSTALL_PREFIX}
@@ -60,15 +60,15 @@ python3 ./NickvisionMoney.GNOME/translate_meta.py ${INSTALL_PREFIX}
 echo "Installing mime types..."
 mkdir -p "${INSTALL_PREFIX}"/share/mime/packages
 cp ./NickvisionMoney.GNOME/org.nickvision.money.extension.xml   \
-   "${INSTALL_PREFIX}"/share/mime/packages/
+    "${INSTALL_PREFIX}"/share/mime/packages/
 update-mime-database "${INSTALL_PREFIX}"/share/mime/
 
 echo "Installing user docs..."
 cd NickvisionMoney.Shared/Docs/yelp
 for lang in *
 do
-	mkdir -p $INSTALL_PREFIX/share/help/$lang/denaro/
-	cp -r $lang/* $INSTALL_PREFIX/share/help/$lang/denaro/
+    mkdir -p $INSTALL_PREFIX/share/help/$lang/denaro/
+    cp -r $lang/* $INSTALL_PREFIX/share/help/$lang/denaro/
 done
 
 echo "Done!"
