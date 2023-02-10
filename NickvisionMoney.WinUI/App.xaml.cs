3caaf9d3ab9f45b6fc41abd25edb3c516bdd514a
Nick Logozzo <nlogozzo225@gmail.com>
2023-02-10 01:25:42 -0500

    V2023.2.0

--- NickvisionMoney.WinUI/App.xaml.cs
+++ NickvisionMoney.WinUI/App.xaml.cs
@@ -27,7 +27,7 @@ public partial class App : Application
         _mainWindowController.AppInfo.Name = "Nickvision Denaro";
         _mainWindowController.AppInfo.ShortName = "Denaro";
         _mainWindowController.AppInfo.Description = $"{_mainWindowController.Localizer["Description"]}.";
-        _mainWindowController.AppInfo.Version = "2023.2.0-rc1";
+        _mainWindowController.AppInfo.Version = "2023.2.0";
         _mainWindowController.AppInfo.Changelog = "- Added the ability to add a password to an account (This will encrypt the nmoney file)\n- Added the ability to transfer money between accounts with different currencies\nby providing a conversion rate in TransferDialog\n- Added the ability to configure how Denaro uses locale separators in amount fields\n- Added the ability to copy individual transactions\n- Added the ability to sort transactions by amount\n- New AccountView design with a collapsible sidebar\n- Recent accounts are now available to select from the TransferDialog\n- Improved importing of qif and ofx file\n- Fixed an issue where dates in TransactionDialog were not displayed with locale settings";
         _mainWindowController.AppInfo.GitHubRepo = new Uri("https://github.com/nlogozzo/NickvisionMoney");
         _mainWindowController.AppInfo.IssueTracker = new Uri("https://github.com/nlogozzo/NickvisionMoney/issues/new");
