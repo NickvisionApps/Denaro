88d452a61967df90a76f7098a581b35a13bf5f4e
Nick Logozzo <nlogozzo225@gmail.com>
2023-02-25 09:35:14 -0500

    V2023.2.2

--- NickvisionMoney.GNOME/Program.cs
+++ NickvisionMoney.GNOME/Program.cs
@@ -51,7 +51,7 @@ public partial class Program
         _mainWindowController.AppInfo.Name = "Nickvision Denaro";
         _mainWindowController.AppInfo.ShortName = "Denaro";
         _mainWindowController.AppInfo.Description = $"{_mainWindowController.Localizer["Description"]}.";
-        _mainWindowController.AppInfo.Version = "2023.3.0-next";
+        _mainWindowController.AppInfo.Version = "2023.2.2";
         _mainWindowController.AppInfo.Changelog = "<ul><li>Improved icon (Thanks @bertob)!</li><li>Various UX improvements</li><li>Updated and added translations (Thanks to everyone on Weblate)!</li></ul>";
         _mainWindowController.AppInfo.GitHubRepo = new Uri("https://github.com/nlogozzo/NickvisionMoney");
         _mainWindowController.AppInfo.IssueTracker = new Uri("https://github.com/nlogozzo/NickvisionMoney/issues/new");
--- NickvisionMoney.WinUI/App.xaml.cs
+++ NickvisionMoney.WinUI/App.xaml.cs
@@ -27,7 +27,7 @@ public partial class App : Application
         _mainWindowController.AppInfo.Name = "Nickvision Denaro";
         _mainWindowController.AppInfo.ShortName = "Denaro";
         _mainWindowController.AppInfo.Description = $"{_mainWindowController.Localizer["Description"]}.";
-        _mainWindowController.AppInfo.Version = "2023.3.0-next";
+        _mainWindowController.AppInfo.Version = "2023.2.2";
         _mainWindowController.AppInfo.Changelog = "- Improved icon (Thanks @bertob)!\n- Various UX improvements\n- Updated and added translations (Thanks to everyone on Weblate)!";
         _mainWindowController.AppInfo.GitHubRepo = new Uri("https://github.com/nlogozzo/NickvisionMoney");
         _mainWindowController.AppInfo.IssueTracker = new Uri("https://github.com/nlogozzo/NickvisionMoney/issues/new");
