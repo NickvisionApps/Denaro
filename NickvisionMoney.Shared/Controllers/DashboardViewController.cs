58eee64997d4067b69abb5f756d4d7e716960eca
Nick Logozzo <nlogozzo225@gmail.com>
2023-03-27 20:14:24 -0400

    Create DashboardViewController.cs

---
+++ NickvisionMoney.Shared/Controllers/DashboardViewController.cs
@@ -0,0 +1,26 @@
+﻿using NickvisionMoney.Shared.Helpers;
+using NickvisionMoney.Shared.Models;
+
+namespace NickvisionMoney.Shared.Controllers;
+
+public class DashboardViewController
+{
+    /// <summary>
+    /// The localizer to get translated strings from
+    /// </summary>
+    public Localizer Localizer { get; init; }
+
+    /// <summary>
+    /// Constructs a DashboardViewController
+    /// </summary>
+    ///  <param name="localizer">The Localizer of the app</param>
+    public DashboardViewController(Localizer localizer)
+    {
+        Localizer = localizer;
+    }
+
+    public void AddAccount(Account account)
+    {
+
+    }
+}
