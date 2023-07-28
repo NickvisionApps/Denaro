9fa349fb1bafd584a4ffcab9d0c46f5451387935
Nick Logozzo <nlogozzo225@gmail.com>
2023-07-28 18:06:40 -0400

    GNOME - Finish Porting Dialogs

--- NickvisionMoney.GNOME/Views/GroupDialog.cs
+++ NickvisionMoney.GNOME/Views/GroupDialog.cs
@@ -1,8 +1,8 @@
+using Nickvision.GirExt;
 using NickvisionMoney.GNOME.Helpers;
 using NickvisionMoney.Shared.Controllers;
 using System;
 using System.Globalization;
-using System.Runtime.InteropServices;
 using static NickvisionMoney.Shared.Helpers.Gettext;
 
 namespace NickvisionMoney.GNOME.Views;
@@ -12,51 +12,19 @@ namespace NickvisionMoney.GNOME.Views;
 /// </summary>
 public partial class GroupDialog : Adw.Window
 {
-    [StructLayout(LayoutKind.Sequential)]
-    public struct Color
-    {
-        public float Red;
-        public float Green;
-        public float Blue;
-        public float Alpha;
-    }
-
-    [LibraryImport("libadwaita-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
-    [return: MarshalAs(UnmanagedType.I1)]
-    private static partial bool gdk_rgba_parse(ref Color rgba, string spec);
-
-    [LibraryImport("libadwaita-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial string gdk_rgba_to_string(ref Color rgba);
-
-    [LibraryImport("libadwaita-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void gtk_color_dialog_button_set_rgba(nint button, ref Color rgba);
-
-    [DllImport("libadwaita-1.so.0")]
-    static extern ref Color gtk_color_dialog_button_get_rgba(nint button);
-
-    [LibraryImport("libadwaita-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void gtk_color_dialog_button_set_dialog(nint button, nint dialog);
-
-    [LibraryImport("libadwaita-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial nint gtk_color_dialog_new();
-
-    [LibraryImport("libadwaita-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void gtk_color_dialog_set_with_alpha(nint dialog, [MarshalAs(UnmanagedType.I1)] bool with_alpha);
-
     private bool _constructing;
     private readonly GroupDialogController _controller;
-    private readonly nint _colorDialog;
+    private readonly Gtk.ColorDialog _colorDialog;
 
     [Gtk.Connect] private readonly Gtk.Label _titleLabel;
     [Gtk.Connect] private readonly Adw.EntryRow _nameRow;
     [Gtk.Connect] private readonly Adw.EntryRow _descriptionRow;
-    [Gtk.Connect] private readonly Gtk.Widget _colorButton;
+    [Gtk.Connect] private readonly Gtk.ColorDialogButton _colorButton;
     [Gtk.Connect] private readonly Gtk.Button _deleteButton;
     [Gtk.Connect] private readonly Gtk.Button _applyButton;
 
     private readonly Gtk.EventControllerKey _nameKeyController;
     private readonly Gtk.EventControllerKey _descriptionKeyController;
-    private readonly Gtk.ShortcutController _shortcutController;
 
     public event EventHandler? OnApply;
     public event EventHandler? OnDelete;
@@ -117,9 +85,9 @@ public partial class GroupDialog : Adw.Window
         _descriptionKeyController.OnKeyPressed += (sender, e) => { if (e.Keyval == 59) { return true; } return false; };
         _descriptionRow.AddController(_descriptionKeyController);
         //Color
-        _colorDialog = gtk_color_dialog_new();
-        gtk_color_dialog_set_with_alpha(_colorDialog, false);
-        gtk_color_dialog_button_set_dialog(_colorButton.Handle, _colorDialog);
+        _colorDialog = Gtk.ColorDialog.New();
+        _colorDialog.SetWithAlpha(false);
+        _colorButton.SetDialog(_colorDialog);
         _colorButton.OnNotify += (sender, e) =>
         {
             if (e.Pspec.GetName() == "rgba")
@@ -135,21 +103,11 @@ public partial class GroupDialog : Adw.Window
         _applyButton.OnClicked += (sender, e) => OnApply?.Invoke(this, EventArgs.Empty);
         _deleteButton.SetVisible(_controller.IsEditing);
         _deleteButton.OnClicked += (sender, e) => OnDelete?.Invoke(this, EventArgs.Empty);
-        //Shortcut Controller
-        _shortcutController = Gtk.ShortcutController.New();
-        _shortcutController.SetScope(Gtk.ShortcutScope.Managed);
-        _shortcutController.AddShortcut(Gtk.Shortcut.New(Gtk.ShortcutTrigger.ParseString("Escape"), Gtk.CallbackAction.New((sender, e) =>
-        {
-            Close();
-            return true;
-        })));
-        AddController(_shortcutController);
         //Load Group
         _nameRow.SetText(_controller.Group.Name);
         _descriptionRow.SetText(_controller.Group.Description);
-        var color = new Color();
-        gdk_rgba_parse(ref color, _controller.Group.RGBA);
-        gtk_color_dialog_button_set_rgba(_colorButton.Handle, ref color);
+        GdkExt.RGBA.Parse(out var color, _controller.Group.RGBA);
+        _colorButton.SetExtRgba(color!.Value);
         Validate();
         _constructing = false;
     }
@@ -168,8 +126,8 @@ public partial class GroupDialog : Adw.Window
     /// </summary>
     private void Validate()
     {
-        var color = gtk_color_dialog_button_get_rgba(_colorButton.Handle);
-        var checkStatus = _controller.UpdateGroup(_nameRow.GetText(), _descriptionRow.GetText(), gdk_rgba_to_string(ref color));
+        var color = _colorButton.GetExtRgba();
+        var checkStatus = _controller.UpdateGroup(_nameRow.GetText(), _descriptionRow.GetText(), color.ToString());
         _nameRow.RemoveCssClass("error");
         _nameRow.SetTitle(_("Name"));
         if (checkStatus == GroupCheckStatus.Valid)
