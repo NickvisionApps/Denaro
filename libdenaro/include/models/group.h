dea81b9137ef5347e72f41dd09e8617640e2b541
JoseBritto <64070234+JoseBritto@users.noreply.github.com>
2024-02-03 15:32:19 -0500

    Add group.h and group.cpp (#781)
    * Add group.h and group.cpp
    
    * Fix styling + add to CMakeLists.txt
    
    * Shared - Add Color to group.h
    
    * Shared - Formatting fixes

---
+++ libdenaro/include/models/group.h
@@ -0,0 +1,105 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+#include <string>
+#include "color.h"
+
+namespace Nickvision::Money::Shared::Models 
+{
+    /**
+     * @brief A model of a group of transactions.
+     */
+    class Group {
+    public:
+        /**
+         * @brief Constructs a Group.
+         * @param id The id of the group
+         */
+        Group(unsigned int id);
+        /*
+         * @brief Gets the id of the group.
+         * @return The id of the group.
+         */
+        unsigned int getId() const;
+        /*
+         * @brief Gets the name of the group.
+         * @return The name of the group.
+         */
+        const std::string& getName() const;
+        /*
+         * @brief Sets the name of the group.
+         * @param name The new name of the group.
+         */
+        void setName(const std::string& name);
+        /*
+         * @brief Gets the description of the group.
+         * @return The description of the group.
+         */
+        const std::string& getDescription() const;
+        /*
+         * @brief Sets the description of the group.
+         * @param description The new description of the group.
+         */
+        void setDescription(const std::string& description);
+        /*
+         * @brief Gets the income of the group.
+         * @return The income of the group.
+         */
+        double getIncome() const;
+        /*
+         * @brief Sets the income of the group.
+         * @param income The new income of the group.
+         */
+        void setIncome(double income);
+        /*
+         * @brief Gets the expense of the group.
+         * @return The expense of the group.
+         */
+        double getExpense() const;
+        /*
+         * @brief Sets the expense of the group.
+         * @param expense The new expense of the group.
+         */
+        void setExpense(double expense);
+        /*
+         * @brief Gets the balance of the group.
+         * @return The balance of the group.
+         */
+        double getBalance() const;
+        /*
+         * @brief Gets the color of the group.
+         * @return The color of the group.
+         */
+        const Color& getColor() const;
+        /*
+         * @brief Sets the color of the group.
+         * @param color The new color of the group.
+         */
+        void setColor(const Color& color);
+        /*
+         * @brief Checks if this object is the same as another group object.
+         */
+        bool operator==(const Group& other) const;
+        /*
+         * @brief Checks if this object is not the same as another group object.
+         */
+        bool operator!=(const Group& other) const;
+        /*
+         * @brief Checks if this object is less than another group object.
+         */
+        bool operator<(const Group& other) const;
+        /*
+         * @brief Checks if this object is greater than another group object.
+         */
+        bool operator>(const Group& other) const;
+        
+    private:
+        unsigned int m_id;
+        std::string m_name;
+        std::string m_description;
+        double m_income;
+        double m_expense;
+        Color m_color;
+    };
+}
+#endif //GROUP_H
---
+++ libdenaro/src/models/group.cpp
@@ -0,0 +1,94 @@
+#include "models/group.h"
+
+namespace Nickvision::Money::Shared::Models 
+{
+        Group::Group(unsigned int id)
+            : m_id{ id },
+            m_name{ "" },
+            m_description{ "" },
+            m_income{ 0.0 },
+            m_expense{ 0.0 }
+        {
+    
+        }
+    
+        unsigned int Group::getId() const
+        {
+            return m_id;
+        }
+    
+        const std::string& Group::getName() const
+        {
+            return m_name;
+        }
+    
+        void Group::setName(const std::string& name)
+        {
+            m_name = name;
+        }
+    
+        const std::string& Group::getDescription() const
+        {
+            return m_description;
+        }
+    
+        void Group::setDescription(const std::string& description)
+        {
+            m_description = description;
+        }
+    
+        double Group::getIncome() const
+        {
+            return m_income;
+        }
+    
+        void Group::setIncome(double income)
+        {
+            m_income = income;
+        }
+    
+        double Group::getExpense() const
+        {
+            return m_expense;
+        }
+    
+        void Group::setExpense(double expense)
+        {
+            m_expense = expense;
+        }
+    
+        double Group::getBalance() const
+        {
+            return m_income - m_expense;
+        }
+        
+        const Color& Group::getColor() const
+        {
+            return m_color;
+        }
+        
+        void Group::setColor(const Color& color)
+        {
+            m_color = color;
+        }
+        
+        bool Group::operator==(const Group& other) const
+        {
+            return m_id == other.m_id;
+        }
+        
+        bool Group::operator!=(const Group& other) const
+        {
+            return m_id != other.m_id;
+        }
+        
+        bool Group::operator<(const Group& other) const
+        {
+            return m_name < other.m_name;
+        }
+        
+        bool Group::operator>(const Group& other) const
+        {
+            return m_name > other.m_name;
+        }
+}
