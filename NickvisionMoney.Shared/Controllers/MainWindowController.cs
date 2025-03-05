5d6129a074f90dd2faa8b111c11fcadb7acfa4e9
britto <jose@britto.tech>
2025-03-04 21:56:54 -0500

    Bump version to 2025.3.0

--- NickvisionMoney.Shared/Controllers/MainWindowController.cs
+++ NickvisionMoney.Shared/Controllers/MainWindowController.cs
@@ -85,7 +85,7 @@ public class MainWindowController : IDisposable
             catch (IOException) { }
             Directory.Delete($"{UserDirectories.Config}{Path.DirectorySeparatorChar}Nickvision{Path.DirectorySeparatorChar}{AppInfo.Name}", true);
         }
-        AppInfo.Version = "2024.2.0";
+        AppInfo.Version = "2025.3.0";
         AppInfo.ShortName = _("Denaro");
         AppInfo.Description = _("Manage your personal finances");
         AppInfo.SourceRepo = new Uri("https://github.com/NickvisionApps/Denaro");
